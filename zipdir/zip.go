97aa1483ebc363d2512c67aa22003b280848c341
ahdekkers <alex@flowfabricator.com>
2022-07-07 11:12:57 +0200

    added unzip function

--- zipdir/zip.go
+++ zipdir/zip.go
@@ -46,6 +46,63 @@ func ZipToDir(inPath, outPath string) error {
 	return nil
 }
 
+type File struct {
+	Name string
+	Data []byte
+}
+
+func UnzipToDir(destDir string, zipData []byte) error {
+	files, err := UnzipToFileData(zipData)
+
+	for _, file := range files {
+		err = os.MkdirAll(destDir, os.ModePerm)
+		if err != nil {
+			return fmt.Errorf("failed to make dirs '%s': %v", destDir, err)
+		}
+
+		path := filepath.Join(destDir, file.Name)
+		outFile, err := os.OpenFile(path, os.O_CREATE|os.O_TRUNC|os.O_RDWR, os.ModePerm)
+		if err != nil {
+			if outFile != nil {
+				outFile.Close()
+			}
+			return fmt.Errorf("failed to open or create file '%s': %v", path, err)
+		}
+
+		_, err = outFile.Write(file.Data)
+		if err != nil {
+			return fmt.Errorf("failed to write data to file '%s': %v", path, err)
+		}
+	}
+	return nil
+}
+
+func UnzipToFileData(zipData []byte) ([]File, error) {
+	zipReader, err := zip.NewReader(bytes.NewReader(zipData), int64(len(zipData)))
+	if err != nil {
+		return nil, fmt.Errorf("failed to read zip data: %v", err)
+	}
+
+	var files []File
+	for _, file := range zipReader.File {
+		fileReader, err := file.Open()
+		if err != nil {
+			return nil, fmt.Errorf("failed to unzip file '%s': %v", file.Name, err)
+		}
+
+		data, err := ioutil.ReadAll(fileReader)
+		fileReader.Close()
+		if err != nil {
+			return nil, fmt.Errorf("failed to read file data for file '%s': %v", file.Name, err)
+		}
+		files = append(files, File{
+			Name: file.Name,
+			Data: data,
+		})
+	}
+	return files, nil
+}
+
 func doZip(sourcePath, targetPath string, entries []os.DirEntry, writer *zip.Writer) error {
 	for _, entry := range entries {
 		sourceName := filepath.Join(sourcePath, entry.Name())
